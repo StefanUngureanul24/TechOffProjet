1750a705e8e5c18ea8b63e69697f871370debe2d
StefanUngureanul24 <unsugreanu_stefan_lucian@yahoo.com>
2022-02-23 23:44:14 +0100

    Mise à jour de routes, attribuer les fonctions définies dans controllers au router

--- routes/objets.js
+++ routes/objets.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const objetCtrl = require('../controllers/objets');
 
 router.get('/', objetCtrl.getAllStuff);
+router.post('/', objetCtrl.createObject);
+router.get('/:id', objetCtrl.getOneThing);
+router.put('/:id', objetCtrl.modifyThing);
+router.delete('/:id', objetCtrl.deleteThing);
 
 module.exports = router;
