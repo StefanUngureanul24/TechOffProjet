48c7fa4b21d517fc8240b7ff1f3b5bee85663ab3
StefanUngureanul24 <unsugreanu_stefan_lucian@yahoo.com>
2022-02-23 20:41:03 +0100

    Modifications sur le fonctionnement du serveur et légères ameliorations du code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 /*
 app.use((req, res, next) => {
     console.log('Requête reçue!');
@@ -21,6 +23,13 @@ app.use((req, res, next) => {
     console.log('Réponse envoyée avec succès !');
 });
 */
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use('/api/donnees', (req, res, next) => {
     const donnees = [
@@ -61,5 +70,14 @@ app.use('/api/donnees', (req, res, next) => {
         },
     ];
 });
+
+/*
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+});
+*/
 
 module.exports = app;
